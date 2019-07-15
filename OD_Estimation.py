d97006eb030729ed8729474ebe9bd7e53ff9f03b
natemay99 <52239000+natemay99@users.noreply.github.com>
2019-07-15 01:56:44 -0700

    Add files via upload

--- OD_Estimation.py
+++ OD_Estimation.py
@@ -3,6 +3,13 @@ from plot_printgraph import PrintGraph
 
 def od_Estimation(G):
 
+    #remove waiting edges from our graph
+    waiting_edges = [];
+    for u,v in G.edges():
+        if G[u][v]['num_passengers'] == 0:
+            waiting_edges.append('('+u+','+v+')')
+    G.remove_edges_from(waiting_edges)
+
     node_ref = {};#master index for all nodes
     arc_ref = {}; #master index for all arcs
     P = [[0]*G.number_of_nodes()]*G.number_of_nodes(); #proportion matrix - all or none
@@ -13,20 +20,19 @@ def od_Estimation(G):
         node_ref[node] = i;
         i +=1
 
-    #fill rac_ref
-    i = 0
+    #fill arc_ref
+
     for (u,v,c) in G.edges.data('num_passengers'):
-        arc_ref[u + '-->' + v] = [i, c];
+        arc_ref[u + '-->' + v] = [c];
 
-        print(u + '-->' + v + ': '+str(i)+', '+str(c))
+        #print(u + '-->' + v + ': '+str(i)+', '+str(c))
+    print(arc_ref)
 
-        i += 1
-
-    #paths = dict(nx.all_pairs_shortest_path(G))
+    paths = dict(nx.all_pairs_shortest_path(G))
 
     #compute proportions by finding shortest paths
-    #for source in paths:
-        #for sink in paths[source]:
-            #P[node_ref[source]][node_ref[sink]] = paths[source][sink]
+    for source in paths:
+        for sink in paths[source]:
+            P[node_ref[source]][node_ref[sink]] = paths[source][sink]
 
-    #print(P)
+    print(P)
