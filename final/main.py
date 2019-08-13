615d7fec16ed8534dafd4cc926c103d6be0d5cf1
Phan Trung Hai Nguyen <28689280+hainguyen93@users.noreply.github.com>
2019-08-13 18:32:11 +0200

    added option not to use heuristic

--- final/main.py
+++ final/main.py
@@ -1,7 +1,7 @@
 """Main codes for preducing the inspection schedules for multiple inspectors
 
-INVOCATION:
-$ python3 main.py timetable chosenDay inspectorFile maxInspectors delta outputFile [--load-od]
+INVOCATION
+$ python3 main.py timetable chosenDay inspectorFile maxInspectors delta MIPGAP outputFile [--load-od]
 
 timetable -- name of the XML file from which train timetable is extracted
             (note: must be in English, otherwise use xmltranslator.py to translate to English).
@@ -10,11 +10,13 @@ inspectorFile -- name of the CSV file from which inspector data is extracted.
 maxInspectors -- maximum number of inspectors allowed to work on the chosen day.
 outputFile -- name of text file, where the produced inspection schedule is stored.
 delta -- incremental step used in heuristic solver.
+MIPGAP -- a floating point between 0 and 1 (e.g., 0.1 (~10%), 0.05 (~5%), etc)
 
-[--load-od] -- options to load od matrix from a file
+[options] -- options to load od matrix from a file (--load-od)
+          -- use the heuristic solver for large scale problem (--heuristic)
 
 EXAMPLE:
-$ python3 main.py EN_GRIPS2019_401.xml Mon inspectors.csv 30 delta schedule.txt [--load-od]
+$ python3 main.py EN_GRIPS2019_401.xml Mon inspectors.csv 30 5 0.10 schedule.txt [--load-od]
 """
 
 import sys
@@ -33,15 +35,15 @@ from readInspectorData import *
 
 def main(argv):
     try:
-        # check cl arguments
-        if len(argv) < 6:
+        if len(argv) < 7:
             raise CLArgumentsNotMatch('ERROR: Command-line arguments do not match')
 
         timetable_file = argv[0]
         chosen_day = argv[1]
         inspector_file = argv[2]
         max_num_inspectors = int(argv[3])
-        outputFile = argv[5]
+        mip_gap = float(argv[5])
+        outputFile = argv[6]
 
         delta = int(argv[4])
         if delta < 1: # check if delta set to 0, if so, reset it to 1
@@ -91,89 +93,91 @@ def main(argv):
         add_sinks_and_source_constraint(graph, model, inspectors, x)
         add_time_flow_constraint(graph, model, inspectors, x)
         minimization_constraint(graph, model, inspectors, OD, shortest_paths, M, x)
-        add_max_num_inspectors_constraint(graph, model, inspectors, 1, x) # default by 1
-
-        known_vars = []  # vars with known solutions
-        #delta = 1 # incremental number of inspector schedules to make
-
-        prev_sols = {} # store values of vars with known solutions
 
         # important for saving constraints and variables
-        model.write("Scheduling.rlp")
-        model.setParam('MIPGap', 0.10)
-        # model.setParam('MIPFocus', 1)
+        model.setParam('MIPGap', mip_gap)
         model.setParam('NumericFocus', 0)
 
-        def mycallback(model, where):
-            if where == GRB.Callback.MIPNODE:
-                model.cbSetSolution(list(prev_sols.keys()), list(prev_sols.values()))
-                model.cbUseSolution()
-                print("MODEL RUNTIME: {}".format(model.cbGet(GRB.Callback.RUNTIME)))
+        if '--heuristic' in argv: # not to use heuristic
+            print('No heuristic')
+            add_max_num_inspectors_constraint(graph, model, inspectors, max_num_inspectors, x)
+            model.write("Scheduling.rlp")
+            model.optimize()
+            solution  = print_solution_paths(inspectors, x)
+        else: # use heuristic solver
+            print('Use heuristic')
+            add_max_num_inspectors_constraint(graph, model, inspectors, 1, x)
+            model.write("Scheduling.rlp")
+            known_vars = []  # vars with known solutions
+            prev_sols = {} # store values of vars with known solutions
 
-        #initial list fill
-        unknown_vars, uncare_vars = update_all_var_lists([], known_vars, depot_dict, prev_sols, x)
+            def mycallback(model, where):
+                if where == GRB.Callback.MIPNODE:
+                    model.cbSetSolution(list(prev_sols.keys()), list(prev_sols.values()))
+                    model.cbUseSolution()
+                    print("MODEL RUNTIME: {}".format(model.cbGet(GRB.Callback.RUNTIME)))
 
-        iteration = 0  # iteration counting
-        new_delta = min(delta, len(depot_dict), max_num_inspectors) # number of inspector to start with
+            #initial list fill
+            unknown_vars, uncare_vars = update_all_var_lists([], known_vars, depot_dict, prev_sols, x)
+            iteration = 0  # iteration counting
+            new_delta = min(delta, len(depot_dict), max_num_inspectors) # number of inspector to start with
+            i = new_delta
 
-        i = new_delta
+            while True:
+                iteration += 1
+                print('=============== ITERATION No.{} ================'.format(iteration))
+                print('''Heuristic Solver is trying to find the
+                      best possible schedule for {} inspector(s) from a set of {}
+                      inspector(s) (all in Known_Vars and Unknown_Vars), where {} of them
+                      are fixed. Other inspectors are set
+                      to 0 (using Gurobi cbSetSolution() function)
+                      '''.format(new_delta,len(known_vars)+len(unknown_vars),len(known_vars)))
+                print('Known Vars: ', known_vars)
+                print('Unknown Vars: ', unknown_vars)
+                print("Don't care Vars: ", uncare_vars)
 
-        while True:
-            iteration += 1
+                for uncare_inspector_id in uncare_vars:
+                    arcs = x.select('*', '*', uncare_inspector_id)
+                    prev_sols.update({arc:0 for arc in arcs})
 
-            print('=============== ITERATION No.{} ================'.format(iteration))
-            print('''Heuristic Solver is trying to find the
-                  best possible schedule for {} inspector(s) from a set of {}
-                  inspector(s) (all in Known_Vars and Unknown_Vars), where {} of them
-                  are fixed. Other inspectors are set
-                  to 0 (using Gurobi cbSetSolution() function)
-                  '''.format(new_delta,len(known_vars)+len(unknown_vars),len(known_vars)))
-            print('Known Vars: ', known_vars)
-            print('Unknown Vars: ', unknown_vars)
-            print("Don't care Vars: ", uncare_vars)
+                update_max_inspectors_constraint(model, i)
+                model.optimize(mycallback)
+                unknown_vars, uncare_vars = update_all_var_lists(unknown_vars, known_vars, depot_dict, prev_sols, x)
 
-            for uncare_inspector_id in uncare_vars:
-                arcs = x.select('*', '*', uncare_inspector_id)
-                prev_sols.update({arc:0 for arc in arcs})
+                if i >= max_num_inspectors:  # termination
+                    break
+                elif i + new_delta > max_num_inspectors:  # last iteration
+                    i = max_num_inspectors
+                else:
+                    i += new_delta
 
-            update_max_inspectors_constraint(model, i)
-            model.optimize(mycallback)
-            unknown_vars, uncare_vars = update_all_var_lists(unknown_vars, known_vars, depot_dict, prev_sols, x)
-
-            if i == max_num_inspectors:  # termination
-                break
-            elif i + new_delta > max_num_inspectors:  # last iteration
-                i = max_num_inspectors
-            else:
-                i += new_delta
-
-        print('=============== Final Solutions =================')
-        print('Inspectors: ', known_vars)
-
-        # write solution to console
-        solution  = print_solution_paths(known_vars, x)
+            print('=============== Final Solutions =================')
+            print('Inspectors: ', known_vars)
+            solution  = print_solution_paths(known_vars, x) # write solution to console
 
         with open(outputFile, 'w') as f:
 	        f.write(solution.to_string())
 
-        #print(solution.to_string())
-
     except CLArgumentsNotMatch as error:
         print(error)
         sys.stderr.write(
-        """USAGE:
-        $ python3 main.py timetable chosenDay inspectorFile maxInspectors delta outputFile
+"""USAGE:
+$ python3 main.py timetable chosenDay inspectorFile maxInspectors delta MIPGAP outputFile [--load-od]
 
-            timetable -- name of the XML file from which train timetable is extracted
-                            (note: must be in English, otherwise use xmltranslator.py to translate to English).
-            chosenDay -- a day to produce inspection shedule (e.g., Mon, Tue, etc).
-            inspectorFile -- name of the CSV file from which inspector data is extracted.
-            maxInspectors -- maximum number of inspectors allowed to work on the chosen day.
-            outputFile -- name of text file, where the produced inspection schedule is stored.
-            delta -- incremental step used in heuristic solver
+    timetable -- name of the XML file from which train timetable is extracted
+            (note: must be in English, otherwise use xmltranslator.py to translate to English).
+    chosenDay -- a day to produce inspection shedule (e.g., Mon, Tue, etc).
+    inspectorFile -- name of the CSV file from which inspector data is extracted.
+    maxInspectors -- maximum number of inspectors allowed to work on the chosen day.
+    outputFile -- name of text file, where the produced inspection schedule is stored.
+    delta -- incremental step used in heuristic solver.
+    MIPGAP -- a floating point between 0 and 1 (e.g., 0.1 (~10%), 0.05 (~5%), etc)
 
-        EXAMPLE:
-        $ python3 main.py EN_GRIPS2019_401.xml Mon inspectors.csv 30 delta schedule.txt [--load-od]\n"""
+    [options] -- options to load od matrix from a file (--load-od)
+              -- use the heuristic solver for large scale problem (--heuristic)
+
+EXAMPLE:
+$ python3 main.py EN_GRIPS2019_401.xml Mon inspectors.csv 30 5 0.10 schedule.txt [--load-od]\n"""
         )
         sys.exit(1)
 
